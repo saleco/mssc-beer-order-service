a41de8283e4bcc60f868a80dd1348f04e314d60a
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-17 22:52:49 +0100

    Calling alocation service and returning the result Listening to the result and dealing with the status changes according to the allocation response

--- src/main/java/guru/sfg/beer/order/service/services/BeerOrderManager.java
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManager.java
@@ -1,6 +1,7 @@
 package guru.sfg.beer.order.service.services;
 
 import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.brewery.model.BeerOrderDto;
 
 import java.util.UUID;
 
@@ -8,4 +9,10 @@ public interface BeerOrderManager {
     BeerOrder newBeerOrder(BeerOrder beerOrder);
 
     void processValidationResult(UUID beerOrderId, Boolean isValid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrderDto);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrderDto);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrderDto);
 }
