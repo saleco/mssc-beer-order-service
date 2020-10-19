0932da1069868970e91b730ef4b66e63cdb63c38
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-19 12:52:26 +0100

    Creating DeallocateOrderAction and DeallocateOrderRequest to send to a queue

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -24,6 +24,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validationFailureAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> allocationFailureAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> allocationPendingAction;
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> deallocateOrderAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -80,8 +81,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                     .event(BeerOrderEventEnum.BEERORDER_PICKED_UP)
                 .and().withExternal()
                     .source(BeerOrderStatusEnum.ALLOCATED).target(BeerOrderStatusEnum.CANCELLED)
-                    .event(BeerOrderEventEnum.CANCEL_ORDER);
-//                    .action(); UPDATE INVENTORY
-
+                    .event(BeerOrderEventEnum.CANCEL_ORDER)
+                    .action(deallocateOrderAction);
     }
 }
