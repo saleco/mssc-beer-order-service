d628fb431ea79c4dcb3aeb67fc8212e7b7b59a68
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-19 11:43:06 +0100

    Testing validation exception status

--- src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
+++ src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
@@ -22,13 +22,18 @@ public class BeerOrderValidationListener {
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
     public void list(Message msg){
 
+        boolean isValid = true;
+
         ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
 
-        System.out.println("########### I RAN ########");
+        //condition to fail validation
+        if("fail-validation".equals(request.getBeerOrder().getCustomerRef())) {
+            isValid = false;
+        }
 
         jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
                 ValidateOrderResult.builder()
-                        .isValid(true)
+                        .isValid(isValid)
                         .orderId(request.getBeerOrder().getId())
                         .build());
 
